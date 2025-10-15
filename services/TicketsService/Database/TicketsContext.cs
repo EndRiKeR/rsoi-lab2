19f77573dba40bd2e5d63d03454af5f60eaa798a
EndRiKeR <41268968+EndRiKeR@users.noreply.github.com>
2025-10-15 19:56:53 +0300

    add migrations
    This reverts commit a65fb0acddd96b7d0a8cfe77d247a641d2e7a181.

--- services/TicketsService/Database/TicketsContext.cs
+++ services/TicketsService/Database/TicketsContext.cs
@@ -8,11 +8,13 @@ public class TicketsContext : DbContext
     public virtual DbSet<Ticket> Tickets { get; set; }
 
     public TicketsContext() { }
+
     public TicketsContext(DbContextOptions<TicketsContext> dbContextOptions) : base(dbContextOptions) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        
         modelBuilder.ApplyConfiguration(new TicketsConfiguration());
     }
 }
