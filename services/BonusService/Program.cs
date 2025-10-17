b9047565cb987ff6301f75ab90636453920b1554
EndRiKeR <41268968+EndRiKeR@users.noreply.github.com>
2025-10-17 14:46:44 +0300

    update programs

--- services/BonusService/Program.cs
+++ services/BonusService/Program.cs
@@ -22,17 +22,17 @@ var app = builder.Build();
 var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var context = services.GetRequiredService<PrivilegeContext>();
-var pendingMigrations = context.Database.GetPendingMigrations().ToList();
-if (pendingMigrations.Any())
-{
-    Console.WriteLine($"Applying {pendingMigrations.Count} migrations...");
-    context.Database.Migrate();
-    Console.WriteLine("Migrations applied successfully");
-}
-else
-{
-    Console.WriteLine("Database is up-to-date");
-}
+// var pendingMigrations = context.Database.GetPendingMigrations().ToList();
+// if (pendingMigrations.Any())
+// {
+//     Console.WriteLine($"Applying {pendingMigrations.Count} migrations...");
+//     context.Database.Migrate();
+//     Console.WriteLine("Migrations applied successfully");
+// }
+// else
+// {
+//     Console.WriteLine("Database is up-to-date");
+// }
 
 // var initDatabaseJob = services.GetRequiredService<InitializeDatabaseJob>();
 // await initDatabaseJob.InitializeDatabaseAsync();
